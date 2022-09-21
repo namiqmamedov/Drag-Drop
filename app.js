fc7987099e290ed9ee8ee4201a277e10547082aa
namiqmamedov <namigmm@code.edu.az>
2022-09-22 03:22:28 +0400

    Drag & Drop

---
+++ app.js
@@ -0,0 +1,43 @@
+// let drag1= document.querySelector('#drag1');
+// let drag2= document.querySelector('#drag2');
+// let drag3 = document.querySelector('#drag3');
+// let Dropzone1 = document.querySelector('#Dropzone1');
+// let Dropzone2 = document.querySelector('#Dropzone2');
+// let Dropzone3 = document.querySelector('#Dropzone3');
+
+
+let dragItems = document.querySelectorAll('.box');
+let Dropzone = document.querySelector('#Dropzone1');
+
+Dropzone1.addEventListener('dragover', function (e) {
+
+    e.preventDefault();
+})
+Dropzone1.addEventListener('dragleave', function (e) {
+
+    e.preventDefault();
+})
+
+for (var i = 0; i < dragItems.length; i++) {
+    dragItems[i].addEventListener('dragstart', function (e) {
+        e.dataTransfer.setData('drag', this.id)
+        document.getElementById('count1').innerHTML = this.id
+    })
+
+}
+
+Dropzone.addEventListener('drop', function (e) {
+    let index = e.dataTransfer.getData('drag');
+    let drgItems = document.getElementById(index);
+    Dropzone.appendChild(drgItems);
+})
+
+
+console.log(dragItems.length);
+// document.getElementById('count').innerHTML = dragItems.length
+
+// backspace (?)
+
+var str = "Hello !";
+str = str.slice(0, -1);
+console.log(str)
